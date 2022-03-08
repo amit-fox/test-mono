712875f0eab24b0ad6c3ba80f88e846235aebeb7
Amit <98324987+amit-fox@users.noreply.github.com>
2022-03-08 17:04:19 +0530

    commit 2

--- packages/package-a/src/app/components/suggestedSearch/suggestedSearchHelpers.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash'
-
-export const groupListContainsName = (stationGroups, name) => {
-  if (!stationGroups || !name) return false
-  return _.find(stationGroups, group => (
-    _.toUpper(_.get(group, 'name')) === _.toUpper(name))
-  )
-}
